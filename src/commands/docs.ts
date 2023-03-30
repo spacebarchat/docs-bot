a730cf4b74c869b8ec295b71a8232e36eff0bcca
Eyad Tamer <eyadtamer2121@gmail.com>
2023-03-30 17:56:31 +0200

    Spacebar rebrand.
    Updated because of rebrand.

--- src/commands/docs.ts
+++ src/commands/docs.ts
@@ -5,7 +5,7 @@ import fetch from "node-fetch";
 import he from "he";
 
 const SEARCH_ENDPOINT = "/search/search_index.json";
-const FOSSCORD_DOCS_BASE_URL = "https://docs.fosscord.com";
+const SPACEBAR_DOCS_BASE_URL = "https://docs.spacebar.chat";
 
 type MKDOCS_RESULT = {
 	location: string;
@@ -54,7 +54,7 @@ const getDocs = async () => {
 		!cache.value ||
 		(cache.fetched?.valueOf() ?? 0) + cache.ttl < Date.now()
 	) {
-		const res = await fetch(FOSSCORD_DOCS_BASE_URL + SEARCH_ENDPOINT);
+		const res = await fetch(SPACEBAR_DOCS_BASE_URL + SEARCH_ENDPOINT);
 		const json = (await res.json()) as MKDOCS_SEARCH_RESULTS;
 
 		json.docs = json.docs
@@ -82,7 +82,7 @@ const getDocs = async () => {
 
 export default {
 	name: "docs",
-	description: "Search the Fosscord documentation",
+	description: "Search the Spacebar documentation",
 	options: [
 		{
 			name: "query",
@@ -99,7 +99,7 @@ export default {
 				: caller.args.join(" ");
 
 		if (!query) {
-			return FOSSCORD_DOCS_EMBED;
+			return SPACEBAR_DOCS_EMBED;
 		}
 
 		const search = await getDocs();
@@ -133,18 +133,18 @@ export default {
 			embeds: [
 				{
 					title: main.title,
-					url: `${FOSSCORD_DOCS_BASE_URL}/${main.location}`,
+					url: `${SPACEBAR_DOCS_BASE_URL}/${main.location}`,
 					description: he.decode(description) + " ...",
 					fields: docs
 						.filter((x) => !!x.text)
 						.map((x) => ({
 							name: x.title,
-							value: `[Link](${FOSSCORD_DOCS_BASE_URL}/${x.location})`,
+							value: `[Link](${SPACEBAR_DOCS_BASE_URL}/${x.location})`,
 							inline: true,
 						}))
 						.slice(0, 4),
 					footer: {
-						text: "https://docs.fosscord.com",
+						text: "https://docs.spacebar.chat",
 					},
 				},
 			],
@@ -164,7 +164,7 @@ export default {
 		const responses = searchResult
 			.map((x) => ({
 				...x,
-				location: new URL(`${FOSSCORD_DOCS_BASE_URL}/${x?.location}`),
+				location: new URL(`${SPACEBAR_DOCS_BASE_URL}/${x?.location}`),
 			}))
 			.map((x) => ({
 				name: `${x.title} - ${x.location.pathname}`.slice(0, 100),
@@ -176,43 +176,43 @@ export default {
 	},
 } as CommandType;
 
-const FOSSCORD_DOCS_EMBED = {
+const SPACEBAR_DOCS_EMBED = {
 	embeds: [
 		{
-			title: "Fosscord Documentation",
-			url: FOSSCORD_DOCS_BASE_URL,
+			title: "Spacebar Documentation",
+			url: SPACEBAR_DOCS_BASE_URL,
 			description:
-				"Fosscord is a free and open source, selfhostable, Discord.com-compatible chat platform.",
+				"Spacebar is a free and open source, selfhostable, Discord.com-compatible chat platform.",
 			fields: [
 				{
 					inline: true,
 					name: "FAQ",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/faq)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/faq)`,
 				},
 				{
 					inline: true,
 					name: "Server Setup",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/server/setup)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/server/setup)`,
 				},
 				{
 					inline: true,
 					name: "Reverse Proxies",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/setup/server/reverseProxy)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/setup/server/reverseProxy)`,
 				},
 				{
 					inline: true,
 					name: "Configuration",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/setup/server/configuration)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/setup/server/configuration)`,
 				},
 				{
 					inline: true,
 					name: "Security",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/setup/server/security/)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/setup/server/security/)`,
 				},
 				{
 					inline: true,
 					name: "Contributing",
-					value: `[Link](${FOSSCORD_DOCS_BASE_URL}/contributing)`,
+					value: `[Link](${SPACEBAR_DOCS_BASE_URL}/contributing)`,
 				},
 			],
 		},
